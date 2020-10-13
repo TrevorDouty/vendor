c9777672d0701bbc219acb3450718d4ace646c7f
TrevorDouty <tdouty@nnu.edu>
2020-10-13 13:34:57 -0600

    init

---
+++ app/Models/Machine.js
@@ -0,0 +1,21 @@
+export default class Machine {
+  constructor() {
+    this.coins = {
+      dollar: 100,
+      quarter: 25,
+      dime: 10,
+      nickel: 5,
+    }
+    this.item = {
+      doritos: "doritos",
+      fritos: "fritos",
+      lays: "lays",
+      skittles: "skittles",
+      snickers: "snickers",
+      cookies: "cookies",
+      crackers: "crackers",
+      mints: "mints",
+      gum: "gum"
+    }
+  }
+}
